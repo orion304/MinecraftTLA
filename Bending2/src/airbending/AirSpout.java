c4a3716a0076594d12afe87df10698259b7d2174
Jonathan <Jonathan@192.168.1.105>
2012-08-12 14:38:00 -0400

    Gave airspout a description

--- Bending2/src/airbending/AirSpout.java
+++ Bending2/src/airbending/AirSpout.java
@@ -1,9 +1,18 @@
 package airbending;
 
+import org.bukkit.entity.Player;
+
 public class AirSpout {
 
+	public AirSpout(Player player) {
+
+	}
+
 	public static String getDescription() {
-		return "Work in progress. It does nothing yet.";
+		return "This ability gives the airbender limited sustained levitation. It is a "
+				+ "toggle - click to activate and form a whirling spout of air "
+				+ "beneath you, lifting you up. You can bend other abilities while using AirSpout. "
+				+ "Click again to deactivate this ability.";
 	}
 
 }
